506edfc23327b7eca9f3850c3c8fe81f7c9d45a3
yasmeenmoham <144394307+yasmeenmoham@users.noreply.github.com>
2023-11-19 14:55:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+   
+    document.querySelector('.btn-book-a-table').addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default behavior of the link
+        // Perform some other action, like navigating to a different page
+        window.location.href = "menu.html";
+    });
+    
